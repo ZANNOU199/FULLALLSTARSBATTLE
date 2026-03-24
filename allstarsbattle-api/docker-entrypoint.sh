e72a073ae083fe537c6956c1322b6483339fcca1
ASBI Togo <zannoharry@gmail.com>
2026-03-24 21:36:01 +0100

    Move MPM config to entrypoint script

--- allstarsbattle-api/docker-entrypoint.sh
+++ allstarsbattle-api/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# Disable conflicting MPM modules
+a2dismod mpm_event mpm_worker || true
+
+# Enable only prefork MPM
+a2enmod mpm_prefork
+
 # Start Apache in foreground
 exec apache2-foreground
