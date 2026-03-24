a77061b4a8ffc9d305698471b0a595f49840cee2
ASBI Togo <zannoharry@gmail.com>
2026-03-24 21:59:22 +0100

    Use PORT environment variable for Apache

--- allstarsbattle-api/docker-entrypoint.sh
+++ allstarsbattle-api/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Set Apache to listen on PORT (default 8080 for Railway)
-export APACHE_PORT=${PORT:-8080}
+# Set Apache to listen on PORT (Railway sets this automatically)
+PORT=${PORT:-8080}
 
 # Update Apache ports configuration
-sed -i "s/Listen 80/Listen ${APACHE_PORT}/g" /etc/apache2/ports.conf
-sed -i "s/:80/:${APACHE_PORT}/g" /etc/apache2/sites-available/000-default.conf
+sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
+sed -i "s/:80/:${PORT}/g" /etc/apache2/sites-available/000-default.conf
 
 # Disable conflicting MPM modules
 a2dismod mpm_event mpm_worker || true
